33ee2a63a35b486179d118adb7db35706d3dd75c
srus <po1onius@outlook.com>
2025-03-16 13:26:43 +0800

    add cpp dev

--- nix/dev/flake.nix
+++ nix/dev/flake.nix
@@ -2,13 +2,21 @@
   description = "rust dev";
 
   inputs = {
-    nixpkgs.url      = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
@@ -16,27 +24,35 @@
         };
       in
       {
-        devShells.default = with pkgs; mkShell rec {
-          buildInputs = [
-            dbus
-            udev
-            alsa-lib-with-plugins
-            vulkan-loader
-            wayland
-            libxkbcommon
-            openssl
-            pkg-config
-            libGL
-            lldb
-            (rust-bin.nightly.latest.default.override {
-              extensions = [ "rust-src" "rust-analyzer" ];
-            })
-          ];
-          LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
-          shellHook = ''
-            export PS1='\[\e[35m\][\w]\[\e[0m\]\$ '
-          '';
-        };
+        devShells.default =
+          with pkgs;
+          mkShell rec {
+            buildInputs = [
+              cmake
+              bear
+              dbus
+              udev
+              alsa-lib-with-plugins
+              vulkan-loader
+              wayland
+              libxkbcommon
+              openssl
+              pkg-config
+              libGL
+              lldb
+              clang-tools
+              (rust-bin.nightly.latest.default.override {
+                extensions = [
+                  "rust-src"
+                  "rust-analyzer"
+                ];
+              })
+            ];
+            # LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+            shellHook = ''
+              export PS1='\[\e[35m\][\w]\[\e[0m\]\$ '
+            '';
+          };
       }
     );
 }
