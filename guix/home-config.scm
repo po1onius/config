39533a44af97798688016a729d2577481f5c44aa
srus <po1onius@outlook.com>
2025-04-13 02:52:26 +0800

    update guix home config

--- guix/home-config.scm
+++ guix/home-config.scm
@@ -1,17 +1,93 @@
 ;; This is a sample Guix Home configuration which can help setup your
 ;; home directory in the same declarative manner as Guix System.
 ;; For more information, see the Home Configuration section of the manual.
-(define-module (guix-home-config)
-  #:use-module (gnu home)
-  #:use-module (gnu home services)
-  #:use-module (gnu home services shells)
-  #:use-module (gnu home services desktop)
-  #:use-module (gnu services)
-  #:use-module (gnu system shadow))
+(use-modules (gnu home)
+	     (gnu home services)
+	     (gnu home services sound)
+	     (gnu packages wm)
+	     (gnu packages ssh)
+	     (gnu packages fcitx5)
+	     (gnu packages gtk)
+	     (gnu packages terminals)
+	     (gnu packages xdisorg)
+	     (gnu packages vim)
+	     (gnu packages freedesktop)
+	     (gnu packages fonts)
+	     (gnu packages version-control)
+	     (gnu services configuration)
+	     (gnu home services shells)
+	     (gnu home services desktop)
+	     (gnu home services shepherd)
+	     (guix gexp)
+	     (guix records)
+	     (nongnu packages chrome)
+	     (nongnu packages fonts)
+	     (gnu services)
+	     (gnu system shadow)
+	     (rosenthal packages binaries))
 
-(define home-config
+
+(define-configuration clash-configuration
+  (clash
+   (file-like mihomo-bin)
+   "The clash package.")
+
+
+  (config-path
+   (string ".config/clash")
+   "config path")
+  (no-serialization))
+
+(define clash-shepherd-service
+  (match-record-lambda <clash-configuration>
+      (clash config-path)
+    (list (shepherd-service
+           (documentation "Run clash.")
+           (provision (list 'clash))
+           (start #~(make-forkexec-constructor
+                     (list (let ((mihomo-cmd
+                                  #$(file-append clash "/bin/mihomo"))
+                                 (clash-cmd
+                                  #$(file-append clash "/bin/clash")))
+                             (if (file-exists? mihomo-cmd)
+                                 mihomo-cmd
+                                 clash-cmd))
+                           "-d" #$config-path)))
+           (stop #~(make-kill-destructor))))))
+
+(define clash-service-type
+  (service-type
+    (name 'clash)
+    (extensions
+      (list (service-extension home-shepherd-service-type
+			       clash-shepherd-service)))
+    (description "clash service")
+    (default-value (clash-configuration))))
+
   (home-environment
+    (packages (list
+		openssh
+		sway
+		gtk+
+		waybar
+		fcitx5
+		fcitx5-qt
+		fcitx5-gtk
+		fcitx5-gtk4
+		fcitx5-configtool
+		fcitx5-chinese-addons
+		fcitx5-material-color-theme
+		alacritty
+		rofi-wayland
+		google-chrome-stable
+		git
+		neovim
+		font-google-noto-emoji
+		font-awesome-nonfree
+		font-intel-one-mono
+		font-lxgw-wenkai-tc))
     (services
+      (append
       (list
         ;; Uncomment the shell you wish to use for your user:
         ;(service home-bash-service-type)
@@ -30,8 +106,11 @@
            ("QT_IM_MODULE" . "fcitx")
            ("QT_PLUGIN_PATH" . "${HOME}/.guix-profile/lib/qt5/plugins")
            ("GUIX_GTK3_IM_MODULE_FILE" . "${HOME}/.guix-profile/lib/gtk-3.0/3.0.0/immodules-gtk3.cache")))
-	(service home-dbus-service-type)
-	))))
+	(service clash-service-type)
+	(service home-pipewire-service-type)
+	(service home-dbus-service-type))
+	%base-home-services)
+	))
 
         ;(service home-files-service-type
         ; `((".guile" ,%default-dotguile)
@@ -41,4 +120,3 @@
         ; `(("gdb/gdbinit" ,%default-gdbinit)
         ;   ("nano/nanorc" ,%default-nanorc)))))))
 
-home-config
