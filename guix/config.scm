2e9cc59d41bcd43dbd627ad80b74bdd98ea5f42c
po1onius <po1onius@outlook.com>
2025-07-21 19:31:56 +0800

    format guix config

--- guix/config.scm
+++ guix/config.scm
@@ -1,75 +1,83 @@
 (use-modules
-  (gnu)
-  (gnu packages shells)
-  (gnu packages linux)
-  (nongnu packages linux)
-  (nongnu system linux-initrd)
-  (gnu services networking)
-  (gnu services desktop)
-  (gnu services sound)
-  (gnu services dbus)
-  (guix channels))
+ (gnu)
+ (gnu packages shells)
+ (gnu packages linux)
+ (nongnu packages linux)
+ (nongnu system linux-initrd)
+ (gnu services networking)
+ (gnu services desktop)
+ (gnu services sound)
+ (gnu services dbus)
+ (guix channels))
 
 
 (operating-system
-  (kernel linux)
-  (firmware (list linux-firmware))
-  (initrd microcode-initrd)
-  (locale "zh_CN.utf8")
-  (timezone "Asia/Shanghai")
-  (keyboard-layout (keyboard-layout "us"))
-  (host-name "guix-PC")
+ (kernel linux)
+ (firmware (list linux-firmware))
+ (initrd microcode-initrd)
+ (locale "zh_CN.utf8")
+ (timezone "Asia/Shanghai")
+ (keyboard-layout (keyboard-layout "us"))
+ (host-name "guix-PC")
 
-  (users (cons (user-account
-		 (name "guix")
-		 (comment "shadow")
-		 (shell (file-append fish "/bin/fish"))
-		 (group "users")
-		 (home-directory "/home/guix")
-		 (supplementary-groups '("wheel" "netdev" "audio" "video" "input")))
-	       %base-user-accounts))
+ (users (cons (user-account
+	       (name "srus")
+	       (comment "srus")
+	       (shell (file-append fish "/bin/fish"))
+	       (group "users")
+	       (home-directory "/home/srus")
+	       (supplementary-groups '("wheel" "netdev" "audio" "video" "input")))
+	      %base-user-accounts))
 
-  (packages %base-packages)
+ (packages %base-packages)
 
-  (services (append (list (service elogind-service-type
-			    (elogind-configuration
-			      (handle-power-key 'ignore)))
-		   (service wpa-supplicant-service-type)
-		   (service bluetooth-service-type)
-		   (service network-manager-service-type)
-			(service polkit-service-type)
-		   (service alsa-service-type))
-		   (modify-services %base-services
-				    (guix-service-type
-				      config => (guix-configuration
-						  (inherit config)
-						  (channels (list
-							      (channel
-								(inherit (car %default-channels))
-								(url "https://codeberg.org/guix/guix-mirror"))
-							      (channel
-							        (name 'rustup)
-								(url "https://github.com/declantsien/guix-rustup"))
-							      (channel
-								   (name 'rosenthal)
-   (url "https://codeberg.org/hako/rosenthal.git")
-   (branch "trunk"))
-							      (channel
-								(name 'nonguix)
-								(url "https://gitlab.com/nonguix/nonguix"))))
-						  (http-proxy "http://127.0.0.1:7890"))))))
+ (services
+  (append
+   (list
+    (service elogind-service-type
+	     (elogind-configuration
+	      (handle-power-key 'ignore)))
+    (service bluetooth-service-type)
+    (service iwd-service-type)
+    (service dhcpcd-service-type)
+    (service polkit-service-type)
+    (service alsa-service-type))
+   (modify-services
+    %base-services
+    (guix-service-type
+     config => (guix-configuration
+		(inherit config)
+		(privileged? #f)
+		(substitute-urls '("https://mirror.sjtu.edu.cn/guix/"))
+		(channels
+		 (append
+		  (list
+		   (channel
+		    (name 'rustup)
+		    (url "https://github.com/declantsien/guix-rustup"))
+		   (channel
+		    (name 'rosenthal)
+		    (url "https://codeberg.org/hako/rosenthal.git")
+		    (branch "trunk"))
+		   (channel
+		    (name 'nonguix)
+		    (url "https://gitlab.com/nonguix/nonguix")))%default-channels))
+		(http-proxy "http://127.0.0.1:7890"))))))
 
-  (bootloader (bootloader-configuration
-                (bootloader grub-efi-bootloader)
-                (targets (list "/boot/efi"))
-                (keyboard-layout keyboard-layout)))
+ (bootloader (bootloader-configuration
+              (bootloader grub-efi-bootloader)
+              (targets (list "/boot/efi"))
+              (keyboard-layout keyboard-layout)))
 
-  (file-systems (append (list (file-system
-                         (mount-point "/boot/efi")
-                         (device (uuid "CB39-D616" 'fat32))
-                         (type "vfat"))
-                       (file-system
-                         (mount-point "/")
-                         (device (uuid "e68d37d0-fd79-450f-af45-56e084d600bf" 'ext4))
-                         (type "ext4")))
-		       %base-file-systems)))
+ (file-systems
+  (append
+   (list
+    (file-system
+     (mount-point "/boot/efi")
+     (device (uuid "7468-8C84" 'fat32))
+     (type "vfat"))
+    (file-system
+     (mount-point "/")
+     (device (uuid "000034b6-7e73-4b29-90bc-cd90dc4a59b2" 'ext4))
+     (type "ext4")))
+   %base-file-systems)))
--- guix/home-config.scm
+++ guix/home-config.scm
@@ -41,88 +41,83 @@
   (no-serialization))
 
 (define clash-shepherd-service
-  (match-record-lambda <clash-configuration>
-      (clash config-path)
-    (list (shepherd-service
-           (documentation "Run clash.")
-           (provision (list 'clash))
-           (start #~(make-forkexec-constructor
-                     (list (let ((mihomo-cmd
-                                  #$(file-append clash "/bin/mihomo"))
-                                 (clash-cmd
-                                  #$(file-append clash "/bin/clash")))
-                             (if (file-exists? mihomo-cmd)
-                                 mihomo-cmd
-                                 clash-cmd))
-                           "-d" #$config-path)))
-           (stop #~(make-kill-destructor))))))
+  (match-record-lambda
+   <clash-configuration>
+   (clash config-path)
+   (list
+    (shepherd-service
+     (documentation "Run clash.")
+     (provision (list 'clash))
+     (start
+      #~(make-forkexec-constructor
+	 (list
+	  (let ((mihomo-cmd
+		 #$(file-append clash "/bin/mihomo"))
+		(clash-cmd
+		 #$(file-append clash "/bin/clash")))
+	    (if (file-exists? mihomo-cmd)
+		mihomo-cmd
+		clash-cmd))
+	  "-d" #$config-path)))
+     (stop #~(make-kill-destructor))))))
 
 (define clash-service-type
   (service-type
-    (name 'clash)
-    (extensions
-      (list (service-extension home-shepherd-service-type
-			       clash-shepherd-service)))
-    (description "clash service")
-    (default-value (clash-configuration))))
+   (name 'clash)
+   (extensions
+    (list (service-extension
+	   home-shepherd-service-type
+	   clash-shepherd-service)))
+   (description "clash service")
+   (default-value (clash-configuration))))
 
-  (home-environment
-    (packages (list
-		openssh
-		sway
-		slurp
-		wl-clipboard
-		grim
-		gtk+
-		waybar
-		fcitx5
-		fcitx5-qt
-		fcitx5-gtk
-		fcitx5-gtk4
-		fcitx5-configtool
-		fcitx5-chinese-addons
-		fcitx5-material-color-theme
-		alacritty
-		rofi-wayland
-		google-chrome-stable
-		git
-		helix
-		bluez
-		font-google-noto-emoji
-		font-awesome-nonfree
-		font-intel-one-mono
-		font-lxgw-wenkai-tc))
-    (services
-      (append
-      (list
-        ;; Uncomment the shell you wish to use for your user:
-        ;(service home-bash-service-type)
-        (service home-fish-service-type)
-        ;(service home-zsh-service-type)
-	(simple-service 'defenv home-environment-variables-service-type
-	   ;;proxy
-	 `(("https_proxy" . "http://127.0.0.1:7890")
-	   ("http_proxy"  . "http://127.0.0.1:7890")
+(home-environment
+ (packages (list
+	    openssh
+	    sway
+	    slurp
+	    wl-clipboard
+	    grim
+	    gtk+
+	    waybar
+	    fcitx5
+	    fcitx5-qt
+	    fcitx5-gtk
+	    fcitx5-gtk4
+	    fcitx5-configtool
+	    fcitx5-chinese-addons
+	    fcitx5-material-color-theme
+	    alacritty
+	    rofi-wayland
+	    google-chrome-stable
+	    git
+	    helix
+	    bluez
+	    font-google-noto-emoji
+	    font-awesome-nonfree
+	    font-apple-sf-mono
+	    font-lxgw-wenkai-tc))
+ (services
+  (append
+   (list
+    (service home-fish-service-type)
+    (simple-service
+     'defenv home-environment-variables-service-type
+     ;;proxy
+     `(("https_proxy" . "http://127.0.0.1:7890")
+       ("http_proxy"  . "http://127.0.0.1:7890")
 
 
-	   ("MOZ_ENABLE_WAYLAND" . "1")
+       ("MOZ_ENABLE_WAYLAND" . "1")
 
-           ;;input method
-           ("GTK_IM_MODULE" . "fcitx")
-           ("QT_IM_MODULE" . "fcitx")
-           ("QT_PLUGIN_PATH" . "${HOME}/.guix-profile/lib/qt5/plugins")
-           ("GUIX_GTK3_IM_MODULE_FILE" . "${HOME}/.guix-profile/lib/gtk-3.0/3.0.0/immodules-gtk3.cache")))
-	(service clash-service-type)
-	(service home-pipewire-service-type)
-	(service home-dbus-service-type))
-	%base-home-services)
-	))
-
-        ;(service home-files-service-type
-        ; `((".guile" ,%default-dotguile)
-        ;   (".Xdefaults" ,%default-xdefaults)))
-
-        ;(service home-xdg-configuration-files-service-type
-        ; `(("gdb/gdbinit" ,%default-gdbinit)
-        ;   ("nano/nanorc" ,%default-nanorc)))))))
+       ;;input method
+       ("GTK_IM_MODULE" . "fcitx")
+       ("QT_IM_MODULE" . "fcitx")
+       ("QT_PLUGIN_PATH" . "${HOME}/.guix-profile/lib/qt5/plugins")
+       ("GUIX_GTK3_IM_MODULE_FILE" . "${HOME}/.guix-profile/lib/gtk-3.0/3.0.0/immodules-gtk3.cache")))
+    (service clash-service-type)
+    (service home-pipewire-service-type)
+    (service home-dbus-service-type))
+   %base-home-services)
+  ))
 
