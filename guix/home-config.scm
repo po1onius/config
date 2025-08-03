dede47217f12177a7bbc18ce081666111bda0903
po1onius <po1onius@outlook.com>
2025-08-03 11:52:00 +0800

    update guix config

--- guix/home-config.scm
+++ guix/home-config.scm
@@ -1,38 +1,39 @@
 ;; This is a sample Guix Home configuration which can help setup your
 ;; home directory in the same declarative manner as Guix System.
 ;; For more information, see the Home Configuration section of the manual.
-(use-modules (gnu home)
-	     (gnu home services)
-	     (gnu home services sound)
-	     (gnu packages)
-	     (gnu packages wm)
-	     (gnu packages image)
-	     (gnu packages ssh)
-	     (gnu packages qt)
-	     (gnu packages fcitx5)
-	     (gnu packages gtk)
-	     (gnu packages terminals)
-	     (gnu packages video)
-	     (gnu packages linux)
-	     (gnu packages xdisorg)
-	     (gnu packages text-editors)
-	     (gnu packages containers)
-	     (gnu packages freedesktop)
-	     (gnu packages fonts)
-	     (gnu packages version-control)
-	     (gnu packages package-management)
-	     (gnu home services shells)
-	     (gnu home services desktop)
-	     (gnu home services shepherd)
-	     (nongnu packages chrome)
-	     (nongnu packages fonts)
-	     (ch0r0ng packages wm)
-	     (gnu services)
-	     (gnu system shadow)
-	     (guix inferior)
-	     (guix channels)
-	     (srfi srfi-1)
-	     (ch0r0ng services network))
+(use-modules
+ (gnu home)
+ (gnu home services)
+ (gnu home services sound)
+ (gnu packages)
+ (gnu packages wm)
+ (gnu packages image)
+ (gnu packages ssh)
+ (gnu packages qt)
+ (gnu packages fcitx5)
+ (gnu packages gtk)
+ (gnu packages terminals)
+ (gnu packages video)
+ (gnu packages linux)
+ (gnu packages xdisorg)
+ (gnu packages text-editors)
+ (gnu packages containers)
+ (gnu packages freedesktop)
+ (gnu packages fonts)
+ (gnu packages version-control)
+ (gnu packages package-management)
+ (gnu home services shells)
+ (gnu home services desktop)
+ (gnu home services shepherd)
+ (nongnu packages chrome)
+ (nongnu packages fonts)
+ (ch0r0ng packages wm)
+ (gnu services)
+ (gnu system shadow)
+ (guix inferior)
+ (guix channels)
+ (srfi srfi-1)
+ (ch0r0ng services network))
 
 
 (define channel-rust-team
@@ -40,11 +41,11 @@
          (name 'guix)
          (url "https://codeberg.org/guix/guix.git")
          (branch "rust-team")
-	 (introduction
-	  (make-channel-introduction
-	   "9edb3f66fd807b096b48283debdcddccfea34bad"
-	   (openpgp-fingerprint
-	    "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))))
+	     (introduction
+	      (make-channel-introduction
+	       "9edb3f66fd807b096b48283debdcddccfea34bad"
+	       (openpgp-fingerprint
+	        "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))))
 
 
 (define inferior-rust-team
@@ -52,41 +53,43 @@
 
 
 (home-environment
- (packages (append (list
-		    openssh
-		    flatpak
-		    obs
-		    obs-wlrobs
-		    sway
-		    mango-git
-		    hyprland
-		    slurp
-		    wl-clipboard
-		    grim
-		    gtk+
-		    waybar-git
-		    fcitx5
-		    fcitx5-qt
-		    fcitx5-gtk
-		    fcitx5-gtk4
-		    fcitx5-configtool
-		    fcitx5-chinese-addons
-		    fcitx5-material-color-theme
-		    alacritty
-		    rofi-wayland
-		    google-chrome-stable
-		    git
-		    bluez
-		    podman-compose
-		    font-google-noto-emoji
-		    font-awesome-nonfree
-		    font-apple-sf-mono
-		    font-lxgw-wenkai-tc
-		    qtwayland)
-		   (map 
-		    (lambda (packs) 
-		      (first (lookup-inferior-packages inferior-rust-team packs)))
-		    '("helix" "niri" "starship"))))
+ (packages
+  (append
+   (list
+	openssh
+	flatpak
+	obs
+	obs-wlrobs
+	sway
+	mango-git
+	hyprland
+	slurp
+	wl-clipboard
+	grim
+	gtk+
+	waybar-git
+	fcitx5
+	fcitx5-qt
+	fcitx5-gtk
+	fcitx5-gtk4
+	fcitx5-configtool
+	fcitx5-chinese-addons
+	fcitx5-material-color-theme
+	alacritty
+	rofi-wayland
+	google-chrome-stable
+	git
+	bluez
+	podman-compose
+	font-google-noto-emoji
+	font-awesome-nonfree
+	font-apple-sf-mono
+	font-lxgw-wenkai-tc
+	qtwayland)
+   (map 
+	(lambda (packs) 
+	  (first (lookup-inferior-packages inferior-rust-team packs)))
+	'("helix" "niri" "starship"))))
 
  
  (services
